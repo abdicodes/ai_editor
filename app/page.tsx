1457b2243cf2791e1e332177119969185a49c5f0
Abdirahman Suldaan <suldaan.89@hotmail.com>
2024-02-03 15:06:27 +0200

    init layers store with zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 'use client'
 import Editor from '@/components/Editor'
 import { ImageStore } from '@/lib/image-store'
+import { LayerStore } from '@/lib/layter-store'
 export default function Home() {
   return (
-    <ImageStore.Provider initialValue={{ generating: false }}>
-      {' '}
-      <main>
-        <Editor />
-      </main>
-    </ImageStore.Provider>
+    <LayerStore.Provider
+      initialValue={{
+        layerComparisonMode: false,
+        layers: [
+          {
+            id: crypto.randomUUID(),
+            url: '',
+            height: 0,
+            width: 0,
+            publicId: '',
+          },
+        ],
+      }}
+    >
+      <ImageStore.Provider initialValue={{ generating: false }}>
+        <main>
+          <Editor />
+        </main>
+      </ImageStore.Provider>
+    </LayerStore.Provider>
   )
 }
